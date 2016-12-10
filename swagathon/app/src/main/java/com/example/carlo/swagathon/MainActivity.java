e5ddfa3c8b4b9241cebc8573d9fb652ad878ec40
cabeywickra <zcabcab@ucl.ac.uk>
2016-12-10 19:00:39 +0000

    UI updated

--- swagathon/app/src/main/java/com/example/carlo/swagathon/MainActivity.java
+++ swagathon/app/src/main/java/com/example/carlo/swagathon/MainActivity.java
@@ -1,8 +1,16 @@
 package com.example.carlo.swagathon;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.SurfaceView;
+import android.view.Window;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
+
 
 import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.CameraBridgeViewBase;
@@ -16,15 +24,15 @@ import org.opencv.imgproc.Imgproc;
 
 public class MainActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2 {
     JavaCameraView myCamera;
-    Mat mRgba, imgGrey , imgCanny;
+    Mat mRgba, imgGrey, imgCanny;
     BaseLoaderCallback mLoaderCallBack = new BaseLoaderCallback(this) {
         @Override
         public void onManagerConnected(int status) {
-            switch(status){
+            switch (status) {
                 case BaseLoaderCallback.SUCCESS:
                     myCamera.enableView();
                     break;
-                default:{
+                default: {
                     super.onManagerConnected(status);
                     break;
                 }
@@ -32,43 +40,50 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
             super.onManagerConnected(status);
         }
     };
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
-
-        myCamera = (JavaCameraView)findViewById(R.id.java_camera_view);
+        myCamera = (JavaCameraView) findViewById(R.id.java_camera_view);
         myCamera.setVisibility(SurfaceView.VISIBLE);
         myCamera.setCvCameraViewListener(this);
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
     @Override
-    protected void onPause(){
+    protected void onPause() {
         super.onPause();
-        if(myCamera != null){
+        if (myCamera != null) {
             myCamera.disableView();
         }
     }
 
     @Override
-    protected void onDestroy(){
+    protected void onDestroy() {
         super.onDestroy();
-        if(myCamera != null){
+        if (myCamera != null) {
             myCamera.disableView();
         }
     }
 
     @Override
-    protected void onResume(){
+    protected void onResume() {
         super.onPause();
-        if(OpenCVLoader.initDebug()){
+        if (OpenCVLoader.initDebug()) {
             mLoaderCallBack.onManagerConnected(LoaderCallbackInterface.SUCCESS);
-        }
-        else{
-            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_1_0,this,mLoaderCallBack);
+        } else {
+            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_1_0, this, mLoaderCallBack);
         }
     }
 
@@ -87,8 +102,46 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
     @Override
     public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
         mRgba = inputFrame.rgba();
-        Imgproc.cvtColor(mRgba,imgGrey,Imgproc.COLOR_RGB2GRAY);
-        Imgproc.Canny(imgGrey,imgCanny,100,150);
+        Imgproc.cvtColor(mRgba, imgGrey, Imgproc.COLOR_RGB2GRAY);
+        Imgproc.Canny(imgGrey, imgCanny, 100, 150);
         return imgCanny;
     }
+
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("Main Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        AppIndex.AppIndexApi.start(client, getIndexApiAction());
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client, getIndexApiAction());
+        client.disconnect();
+    }
 }
+
