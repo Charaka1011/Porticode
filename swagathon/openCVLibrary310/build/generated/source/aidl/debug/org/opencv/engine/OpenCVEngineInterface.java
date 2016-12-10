8df4eb82a9f5e1ff851b011cbf44e603e7afa34b
Carlo Winkelhake <carlo@Carlos-MacBook-Pro.local>
2016-12-10 17:21:19 +0000

    sick icon

--- swagathon/openCVLibrary310/build/generated/source/aidl/debug/org/opencv/engine/OpenCVEngineInterface.java
+++ swagathon/openCVLibrary310/build/generated/source/aidl/debug/org/opencv/engine/OpenCVEngineInterface.java
@@ -1,6 +1,6 @@
 /*
  * This file is auto-generated.  DO NOT MODIFY.
- * Original file: /Users/cabeywickra/Desktop/Porticode/swagathon/openCVLibrary310/src/main/aidl/org/opencv/engine/OpenCVEngineInterface.aidl
+ * Original file: /Users/carlo/Porticode/swagathon/openCVLibrary310/src/main/aidl/org/opencv/engine/OpenCVEngineInterface.aidl
  */
 package org.opencv.engine;
 /**
