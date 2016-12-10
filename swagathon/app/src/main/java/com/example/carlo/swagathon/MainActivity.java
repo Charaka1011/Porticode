7d9c18894283b34ca0d9eedf390aa9c63992a82a
cabeywickra <cabeywickra@eduroam-int-dhcp-97-119-70.ucl.ac.uk>
2016-12-10 15:46:12 +0000

    camera app

--- swagathon/app/src/main/java/com/example/carlo/swagathon/MainActivity.java
+++ swagathon/app/src/main/java/com/example/carlo/swagathon/MainActivity.java
@@ -2,24 +2,88 @@ package com.example.carlo.swagathon;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.SurfaceView;
 
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.JavaCameraView;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
 import org.opencv.android.OpenCVLoader;
 
-public class MainActivity extends AppCompatActivity {
-    private static  String TAG= "MainActivity";
+public class MainActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2 {
+    JavaCameraView myCamera;
+    Mat mRgba;
+    BaseLoaderCallback mLoaderCallBack = new BaseLoaderCallback(this) {
+        @Override
+        public void onManagerConnected(int status) {
+            switch(status){
+                case BaseLoaderCallback.SUCCESS:
+                    myCamera.enableView();
+                    break;
+                default:{
+                    super.onManagerConnected(status);
+                    break;
+                }
+            }
+            super.onManagerConnected(status);
+        }
+    };
 
-    static {
-        if(OpenCVLoader.initDebug()){
-            Log.i(TAG , "OpenCV: Loaded Successfully");
-        }else{
-            Log.i(TAG , "OpenCV: ERROR");
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        myCamera = (JavaCameraView)findViewById(R.id.java_camera_view);
+        myCamera.setVisibility(SurfaceView.VISIBLE);
+        myCamera.setCvCameraViewListener(this);
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        if(myCamera != null){
+            myCamera.disableView();
         }
     }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    protected void onDestroy(){
+        super.onDestroy();
+        if(myCamera != null){
+            myCamera.disableView();
+        }
+    }
+
+    @Override
+    protected void onResume(){
+        super.onPause();
+        if(OpenCVLoader.initDebug()){
+            mLoaderCallBack.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+        }
+        else{
+            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_1_0,this,mLoaderCallBack);
+        }
+    }
+
+    @Override
+    public void onCameraViewStarted(int width, int height) {
+        mRgba = new Mat(height, width, CvType.CV_8UC4);
+    }
+
+    @Override
+    public void onCameraViewStopped() {
+        mRgba.release();
+    }
+
+    @Override
+    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+        mRgba = inputFrame.rgba();
+        return mRgba;
     }
 }
