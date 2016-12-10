2e86db25a1642c172e701a2383854d3c7e206bec
cabeywickra <cabeywickra@eduroam-int-dhcp-97-119-70.ucl.ac.uk>
2016-12-10 13:08:59 +0000

    Getting started

--- swagathon/app/src/main/java/com/example/carlo/swagathon/MainActivity.java
+++ swagathon/app/src/main/java/com/example/carlo/swagathon/MainActivity.java
@@ -2,8 +2,20 @@ package com.example.carlo.swagathon;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import org.opencv.android.OpenCVLoader;
 
 public class MainActivity extends AppCompatActivity {
+    private static  String TAG= "MainActivity";
+
+    static {
+        if(OpenCVLoader.initDebug()){
+            Log.i(TAG , "OpenCV: Loaded Successfully");
+        }else{
+            Log.i(TAG , "OpenCV: ERROR");
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
